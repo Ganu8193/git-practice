8857adcdb5499cb235f5dcf538f6e23fcfc8803e
Ganu8193 <54851000+Ganu8193@users.noreply.github.com>
2022-05-05 22:27:17 +0530

    second commit

---
+++ testgit.cpp
@@ -0,0 +1,12 @@
+# include<iostream>
+using namespace std;
+
+int main(){
+    if(x == hungry){
+        cout << "eat pizza";
+        cout << "eat samosa";asfasdasf
+    }
+    else{
+        cout << "do homework";
+    }
+}
